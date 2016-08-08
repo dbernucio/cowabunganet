4367087ed2042ce2bd78a0d0ade5d517a17a5c41
dbernucio” <dbernucio@caiena.net>
2016-08-08 09:59:12 -0300

    Ajustes finais

--- assets/javascripts/base.js
+++ assets/javascripts/base.js
@@ -1,35 +1,30 @@
-'use strict'
-
 $(function() {
   // menu
   var gNav          = $('.main-nav'),
       gMobileStripe = $('.menu-stripe'),
       gMobileMenu   = $('.mobile-menu');
 
-  gNav.on('click', 'li', function(e){
+  gNav.on('click', 'li', function(e) {
     e.preventDefault();
     $('html,body').scrollTo($(this).find('a').attr('href'), 500);
   });
 
-  gMobileMenu.on('click', function(){
+  gMobileMenu.on('click', function() {
     $(this).fadeToggle();
   });
 
-  gMobileStripe.on('click', function(){
+  gMobileStripe.on('click', function() {
     gMobileMenu.fadeToggle();
   });
 
   // networks
   $('.card').hover(function () {
-    var self = $(this).find('.networkings');
-    $(this).animate({
-        top: '-15px'
-    }, 500);
-    $(self, this).show();
-  }, function () {
-    $(this).animate({
-        top: '0px'
-    }, 500);
-    $(self, this).hide();
+    var self = $(this).find('.networks');
+    if(self.css('visibility') == 'hidden') {
+      self.css({ visibility: 'visible' });
+    }
+    else {
+      self.css({ visibility: 'hidden' });
+    }
   });
 });
