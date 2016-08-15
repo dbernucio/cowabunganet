ee20ea6aee51472fa1c0481148d3854674d9e9ef
dbernucio” <dbernucio@caiena.net>
2016-08-15 13:48:33 -0300

    Adicionando formulário de contato

--- index.php
+++ index.php
@@ -1,3 +1,48 @@
+<?php
+  if (isset($_POST["submit"])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $site = $_POST['site'];
+    $message = $_POST['message'];
+    $human = intval($_POST['human']);
+    $from = 'Demo Contact Form';
+    // $to = 'contato@cowabunganet.esy.es';
+    $to = 'diane.bernucio@gmail.com';
+    $subject = 'Nova mensagem de contato';
+
+    $body ="<b>Nome: $name</b>\n <b>E-mail</b>: $email\n" +
+      "<b>Site: $site</b>\n \n\n $message";
+
+    // Check if name has been entered
+    if (!$_POST['name']) {
+      $errName = 'Please enter your name';
+    }
+
+    // Check if email has been entered and is valid
+    if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+      $errEmail = 'Please enter a valid email address';
+    }
+
+    //Check if message has been entered
+    if (!$_POST['message']) {
+      $errMessage = 'Please enter your message';
+    }
+    //Check if simple anti-bot test is correct
+    if ($human !== 5) {
+      $errHuman = 'Your anti-spam is incorrect';
+    }
+    // If there are no errors, send the email
+    if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
+      if (mail ($to, $subject, $body, $from)) {
+        $result='<div class="alert alert-success">Obrigado! Entraremos em contato com você!</div>';
+      }
+      else {
+        $result='<div class="alert alert-danger">Ocorreu um erro ao enviar a sua mensagem. Tente novamente.</div>';
+      }
+    }
+  }
+?>
+
 <html lang="pt-br">
   <head>
     <meta charset="utf-8">
@@ -21,10 +66,12 @@
         </div>
         <ul class="menu" id="floating-menu">
           <li><a href="#media">Mídias</a></li>
+          <li><a href="#contact">Contato</a></li>
           <li><a href="#who">Quem somos</a></li>
         </ul>
         <ul class="mobile-menu">
           <li><a href="#media">Mídias</a></li>
+          <li><a href="#contact">Contato</a></li>
           <li><a href="#who">Quem somos</a></li>
         </ul>
       </nav>
@@ -115,6 +162,52 @@
             </div>
           </div>
         </div>
+      </section>
+    </div>
+
+    <!-- Contact -->
+
+    <div id="contact">
+      <section>
+        <form class="form-horizontal" role="form" method="post" action="index.php">
+          <div class="form-group">
+            <div class="col-sm-10">
+              <input type="text" class="form-control" id="name" name="name" placeholder="Nome" value="">
+            </div>
+          </div>
+          <div class="form-group">
+            <div class="col-sm-10">
+              <input type="email" class="form-control" id="email" name="email" placeholder="Email: exemplo@exemplo.com" value="">
+            </div>
+          </div>
+          <div class="form-group">
+            <div class="col-sm-10">
+              <input type="text" class="form-control" id="site" name="site" placeholder="Site" value="">
+            </div>
+          </div>
+          <div class="form-group">
+            <label for="message" class="col-sm-2 control-label">Mensagem</label>
+            <div class="col-sm-10">
+              <textarea class="form-control" rows="4" name="message"></textarea>
+            </div>
+          </div>
+          <div class="form-group">
+            <label for="human" class="col-sm-2 control-label">2 + 3 = ?</label>
+            <div class="col-sm-10">
+              <input type="text" class="form-control" id="human" name="human" placeholder="Your Answer">
+            </div>
+          </div>
+          <div class="form-group">
+            <div class="col-sm-10 col-sm-offset-2">
+              <input id="submit" name="submit" type="submit" value="Enviar" class="btn btn-primary">
+            </div>
+          </div>
+          <div class="form-group">
+            <div class="col-sm-10 col-sm-offset-2">
+              <! Will be used to display an alert to the user>
+            </div>
+          </div>
+        </form>
       </section>
     </div>
 
